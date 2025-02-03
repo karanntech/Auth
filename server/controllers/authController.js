7550c06dab302c7bac8279775f8075cfb6417ad0
karann09 <karankumar011kp@gmail.com>
2025-02-03 19:07:36 +0530

    created login controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -32,3 +32,36 @@ export const register = async(req, res)=> {
         res.json({success: false, message: error.message})
     }
 }
+
+export const login = async(req,res)=>{
+    const {email, password} = req.body;
+
+    if(!email || !password){
+        return res.json({success: false, message: "Email and Password are required"})
+    }
+
+    try {
+        const user = await User.findOne({email});
+        if(!user){
+            return res.json({success: false, message: "Invalid Email"})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+           return res.json({success: false, message: "Invalid password"})
+        }
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn:'7d'});
+
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            maxAge: 7*24*60*60*1000
+        });
+
+        return res.json({success: true, })
+
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
+}
