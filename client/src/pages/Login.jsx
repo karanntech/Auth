58d7d0a1b0ab269422f416d84589dd9cb08405e8
karann09 <karankumar011kp@gmail.com>
2025-02-19 20:31:26 +0530

    created pages

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { assets } from '../assets/assets';
+
 
 const Login = () => {
+  
+  const [state, setState] = useState('Sign Up')
   return (
-    <div>
-        <h1>Login</h1>
+    <div className='flex items-center justify-center'>
+        <img src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer' />
+        <div>
+          <h2>{state === 'Sign Up' ? 'Create account' : 'Login'}</h2>
+          <p>{state === 'Sign Up' ? 'Create your account' : 'Login to your account!'}</p>
+        </div>
     </div>
   )
 }
